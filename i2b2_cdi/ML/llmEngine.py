cc559401bfffa4e8d610de53af8c0742ff888d91
Kavishwar Wagholikar <waghsk@gmail.com>
2025-06-10 13:52:20 +0000

    to refactor engine

--- i2b2_cdi/ML/llmEngine.py
+++ i2b2_cdi/ML/llmEngine.py
@@ -9,25 +9,12 @@ import shutil
 
 def llm_apply(conceptPath,conceptCode,crc_ds,jobId):
     logger.info('Running job LLM-llm_apply {} for conceptPath:{}',jobId,conceptPath)
-    outDir=Path('/usr/src/app/tmp/LLM/output')
-    outDir.mkdir(parents=True, exist_ok=True)
-    fpath=str(outDir)+'/llm_{}_facts.csv'.format(conceptCode)
-    factLoad = ['fact','load','-i','/usr/src/app/tmp/LLM/output']
-
-    arr = []
+    arr=[]
     for patient_num in ['12345']:
         arr.append([patient_num,conceptCode,'1970-01-01 00:00:00',''])
-        
-    pd.DataFrame(arr,columns=['mrn','code','start-date','value']).to_csv(fpath,index=False)
-
-    import i2b2_cdi.fact.runner as fact_runner
-    logger.debug(factLoad)
-
-    config = Config().new_config(argv=factLoad)
-    fact_runner.mod_run(config)
-    Path(fpath).unlink()
-    
-    return
+    df=pd.DataFrame(arr,columns=['mrn','code','start-date','value'])
+    #Engine.load_facts(df)
+    return df
 
 class llmEngine(BaseEngine):
     def __init__(self):
@@ -38,6 +25,6 @@ class llmEngine(BaseEngine):
     def run(self, jobId, projectName, input, conceptCode, conceptPath, node, jobType):
         logger.info(jobType)
         functionName=input['function']
-        #functionName = jobType.replace("llm-","").lower()
-        globals()[functionName](conceptPath,conceptCode,self.crc_ds,jobId)
-
+        df=globals()[functionName](conceptPath,conceptCode,self.crc_ds,jobId)
+        self.load_facts(df)
+        return
--- i2b2_cdi/job/BaseEngine.py
+++ i2b2_cdi/job/BaseEngine.py
@@ -1,7 +1,30 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+import pandas as pd
+from i2b2_cdi.database.cdi_database_connections import I2b2crcDataSource
+from i2b2_cdi.config.config import Config
+from loguru import logger
 
 class BaseEngine(ABC):
 
     @abstractmethod
     def run(self, jobId, projectName, input, conceptCode, conceptPath, host, jobType):
         pass
+
+    def load_facts(self,df):
+        class_name = self.__class__.__name__
+        outDir='/usr/src/app/tmp/{}/output'.format(class_name)
+        Path(outDir).mkdir(parents=True, exist_ok=True)
+        fpath=outDir+'/llm_{}_facts.csv'.format(class_name)
+        factLoad = ['fact','load','-i',outDir]
+
+            
+        df.to_csv(fpath,index=False)
+
+        import i2b2_cdi.fact.runner as fact_runner
+        logger.debug(factLoad)
+
+        config = Config().new_config(argv=factLoad)
+        fact_runner.mod_run(config)
+        Path(fpath).unlink()
+        return 
