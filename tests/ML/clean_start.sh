1f9f99a2ab383230498a5de20350d35985807a35
Kavishwar Wagholikar <waghsk@gmail.com>
2025-07-22 00:12:26 +0000

    works on pima

--- tests/ML/clean_start.sh
+++ tests/ML/clean_start.sh
@@ -1,5 +1,7 @@
+cd ../../
+docker build -t i2b2/i2b2-etl:local-v1 . 
 cd deployment/pg
 docker rm -f $(docker ps -aq)
 docker volume rm $(docker volume ls -q)
 docker compose up -d i2b2-ml i2b2-jupyter
-docker rm -f i2b2-ml; docker-compose up -d  i2b2-ml;  docker-compose logs -f i2b2-ml
+docker rm -f i2b2-ml; docker compose up -d  i2b2-ml;  docker logs -f i2b2-ml
